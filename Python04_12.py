175647d283ed335e8e7f40fec96c681924599e5a
IgorCalixto007 <igorcalixto.pira@gmail.com>
2026-08-27 19:20:48 -0300

    Delete Python04_12.py

--- Python04_12.py
+++
@@ -1,5 +0,0 @@
-list = ['ATGCCCGGCCCGGC','GCGTGCTAGCAATACGATAAACCGG', 'ATATATATCGAT','ATGGGCCC']
-#compreensao de lista que cria uma lista de tuplas com base no comprimento de cada string
-t_list = [(len(i),i) for i in list]
-for i in t_list:
-    print("{}\t{}\n".format(i[0],i[1]))
