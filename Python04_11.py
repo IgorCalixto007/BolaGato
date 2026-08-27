d0713505bf8cbfd86661832cc83a0139d86fb779
IgorCalixto007 <igorcalixto.pira@gmail.com>
2026-08-27 19:20:31 -0300

    Delete Python04_11.py

--- Python04_11.py
+++
@@ -1,6 +0,0 @@
-list = ['ATGCCCGGCCCGGC','GCGTGCTAGCAATACGATAAACCGG', 'ATATATATCGAT','ATGGGCCC']
-for i in list:
-    #metodo .format() permite organizar o output
-    print('{:>3}  {:>}'.format(len(i), i))
-
-
